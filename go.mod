5fa963bc00859642f9a103b3f117ba855473a196
Henri LEPIC <henrilepic@gmail.com>
2020-12-03 09:56:29 +0100

    add config file

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/spf13/viper v1.7.1
 	github.com/ugorji/go v1.2.0 // indirect
 	golang.org/x/crypto v0.0.0-20201124201722-c8d3bf9c5392 // indirect
 	golang.org/x/sys v0.0.0-20201130072748-111129e158e2 // indirect
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 
 	"movie/concurrency"
 	"movie/db/moke"
@@ -9,9 +10,28 @@ import (
 	"movie/service"
 )
 
+type Config struct {
+	MySigningKey string
+}
+
+var conf Config
+
+func init() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	viper.SetDefault("MySigningKey", "AFnakljdfhskjhdfsffk")
+	conf.MySigningKey = viper.GetString("MySigningKey")
+}
+
 func main() {
+
 	concurrency.Service()
-	middleware.MySigningKey = []byte("als;dkjf;lasdjfl;saj")
+	middleware.MySigningKey = []byte(conf.MySigningKey)
 	r := gin.Default()
 	db := moke.NewMokeDB()
 	//db := sqlite.New()
